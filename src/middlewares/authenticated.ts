71cdddee0d3bd01d74c6afe0cdda60018a024792
Denis Eduardo <dandenisedu@gmail.com>
2025-02-25 12:56:43 -0400

    feat(token) - metodo de token para controle de acesso

---
+++ src/middlewares/authenticated.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface Payload{
+    sub: string;
+}
+
+export function authenticated(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({
+            message: "Token missing"
+        });
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+        const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
+
+        return next();
+    } catch (err) {
+        return res.status(401).json({
+            message: "Invalid token"
+        });
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,9 @@ import { Router, Request, Response, NextFunction } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController"
 import { AuthUserController } from "./controllers/user/AuthUserController"
+import { DetailUserController } from "./controllers/user/DetailUserController"
+
+import { authenticated } from "./middlewares/authenticated"
 
 const router = Router();
 
@@ -13,6 +16,11 @@ router.post("/users", async (req: Request, res: Response): Promise<void> => {
 router.post("/session", async (req: Request, res: Response): Promise<void> => {
     const authUserController = new AuthUserController();
     await authUserController.handle(req, res);
+})
+
+router.get("/me", (req, res, next) => {authenticated(req, res, next)}, async (req: Request, res: Response): Promise<void> => {
+    const detailUserController = new DetailUserController();
+    await detailUserController.handle(req, res);
 })
 
 export { router };
